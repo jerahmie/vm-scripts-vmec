f2b1f6d6c1c8bacbbcedc2cbca654dc5e3dbe045
Jerahmie Radder <jerahmie@uwalumni.com>
2014-06-26 23:40:44 -0500

    debugging vmec_checksum.sh

--- vm-debian/scripts/vmec_checksum.sh
+++ vm-debian/scripts/vmec_checksum.sh
@@ -9,10 +9,10 @@ REGEX_LINUX_MD5SUM='^\([0-9a-f]*\)[ ]*stellinstall.zip$'
 REGEX_MACOSX_MD5SUM='^MD5 (stellinstall.zip) = \([0-9a-f]*\)$'
 
 md5sum_stellinstall_local=$($MD5SUM_BINARY $VMEC_ZIP | sed 's/$REGEX_LINUX_MD5SUM/poop/')
-echo $md5sum_vmec_zip
+echo "md5sum_stellinstall_local: $md5sum_stellinstall_local"
 
 md5sum_stellinstall_server=$(cat md5sum.txt | sed 's/$REGEX_MACOSX_MD5SUM/fart/')
-echo $md5sum_stellinstall_server
+echo "md5sum_stellinstall_server: $md5sum_stellinstall_server"
 
 if [[ $md5sum_stellinstall_local == $md5sum_stellinstall_server ]]; then
     echo "MD5 sums were a match"
