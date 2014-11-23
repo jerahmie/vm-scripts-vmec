03f9e111c2cbed4e2e3dc0046539c960df67aa88
Jerahmie Radder <jerahmie@uwalumni.com>
2014-11-22 19:21:04 -0600

    Fixing fdfpp build script and added test for successful fdfpp module build.

--- vm-debian/scripts/build_fdfpp.sh
+++ vm-debian/scripts/build_fdfpp.sh
@@ -29,6 +29,7 @@ if [ -d "$BUILD_DIR" ]; then
 else
     $GIT clone $FDFPP_URL fdfpp
     cd $BUILD_DIR
+fi
 
 # Build and install
 cmake . || { echo "CMake FDFPP failed."; exit 1; }
@@ -36,3 +37,4 @@ make || { echo "FDFPP build failed."; exit 1; }
 make install || { echo "FDFPP install failed."; exit 1; }
 $PYTHON setup.py build_ext  || { echo "FDFPP python extension build failed."; exit 1; }
 $PYTHON setup.py install || { echo "FDFPP python extension install failed."; exit 1; }
+$PYTHON -c "import fdfpp" || { echo "Failed to import fdfpp Python module."; exit 1; }
